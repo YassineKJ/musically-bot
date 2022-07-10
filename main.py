c53632184d76af4928be21a5b560f1c227ae1fb9
YassineKJ <109021048+YassineKJ@users.noreply.github.com>
2022-07-10 10:43:26 +0100

    Discord music bot

---
+++ main.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+import music
+
+client = commands.Bot(command_prefix = '?')
+
+cogs = [music]
+for i in range(len(cogs)):
+  cogs[i].setup(client)
+
+client.run("OTkyNDkxNDkxMTY1NDg3MTY0.GiJESI._rK-JpOSW4uCQM-7tM9u2c5-v7e-_wmtsdFo7c")
