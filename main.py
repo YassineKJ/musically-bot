66b9e6ae3ed07ef2d9f8a476591be6b6367dbcf0
YassineKJ <109021048+YassineKJ@users.noreply.github.com>
2022-07-10 12:27:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import music
 
-client = commands.Bot(command_prefix = '?', intents=discord.Intetns.all())
+client = commands.Bot(command_prefix = '?', intents=discord.Intents.all())
 
 cogs = [music]
 for i in range(len(cogs)):
